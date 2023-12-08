0a1703b6eeef6f23d5b67fe3b88d54fb0f30f56c
Mathieu Ducrot <mathieu.ducrot@smartbooster.io>
2023-12-08 14:37:40 +0100

    Redefined orm and update Makefile commands using the Double-Colon Rules

--- make/install.mk
+++ make/install.mk
@@ -1,6 +1,8 @@
-# Fix rights of var & public/files folders
+##
+## Installation and update
+## -------
 .PHONY: init-rw-files
-init-rw-files:
+init-rw-files: ## Fix rights of var & public/files folders
 	sudo mkdir -p ./var/cache
 	sudo chmod -R 777 ./var/cache
 	sudo chmod -R 777 ./var/log
@@ -10,7 +12,7 @@ init-rw-files:
 
 SYMFONY_INSTALL_VERSION?=lts
 .PHONY: install-symfony
-install-symfony:
+install-symfony: ## Install a new fresh version of symfony
 	make check-missing-env
 	# Adding env variables for doctrine
 	echo "" >> .env
@@ -38,5 +40,28 @@ install-symfony:
 	echo Install complete!
 
 .PHONY: remove-symfony
-remove-symfony:
+remove-symfony: ## Remove all symfony related files of the repository
 	sudo rm -rf bin config migrations node_modules public project src templates tests translations vendor ./var/cache ./var/log/php/* composer.json composer.lock phpunit.xml.dist symfony.lock
+
+.PHONY: install
+install: ## Install the project. MUST BY RUN OUTSIDE OF THE DOCKER CONTAINER !
+ifeq (,$(wildcard composer.json)) # If no composer.json then we install Symfony
+	make remove-symfony # MDT to be sure the install works, we delete the potential existing SF files before redoing the installation
+	make install-symfony
+else
+	make init-rw-files
+	docker compose exec --user=dev php make install-script
+endif
+
+.PHONY: install-script
+install-script: ## Install script using project packages config and setting up db (must be call on the php container, use it also on CI)
+	composer install
+	yarn install
+	make assets-dev
+	# Stopping the install at this step if the doctrine-fixtures-bundle is not installed or if the minimal/fake groups are not defined
+	make orm-install
+	make orm-load-fake
+	echo Install complete!
+
+.PHONY: update
+update:: orm-update orm-status ## Update the project (database migration, custom project command, ...)
