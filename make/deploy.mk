53f02662405cf704cab58a7e3cd2fdf3361c26b0
Mathieu Ducrot <mathieu.ducrot@smartbooster.io>
2024-02-06 10:27:29 +0100

    Add `main` except branch for test job + Remove deploy.mk GIT_BRANCH unused variable

--- make/deploy.mk
+++ make/deploy.mk
@@ -1,12 +1,6 @@
 ##
 ## Deploy
 ## ----------
-ifeq ($(shell git name-rev --name-only HEAD),remotes/origin/HEAD)
-	GIT_BRANCH=master
-else
-	GIT_BRANCH=$(shell git name-rev --name-only HEAD)
-endif
-
 .PHONY: deploy-revision
 deploy-revision: ## Create deploy revision file
 	touch public/revision.txt
